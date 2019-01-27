4f9e932889172414735db97435309494cf6e15d2
marina60394 <marina60394@gmail.com>
2019-01-28 00:30:20 +0200

    add tasks 02 and 04

---
+++ src/Tasks/Task_02_Variables.java
@@ -0,0 +1,19 @@
+package Tasks;
+
+/**
+ * Created by Marina on 27.01.2019.
+ */
+public class Task_02_Variables {
+    public static void main(String[] args) {
+        int intVariable=1;
+        double doubleVariable;
+        String stringVariable;
+
+        stringVariable=Integer.toString(intVariable);
+        doubleVariable=Double.parseDouble(stringVariable);
+        intVariable=(int)doubleVariable;
+        System.out.println("String value: "+stringVariable);
+        System.out.println("double value :"+doubleVariable);
+        System.out.println("int value "+intVariable);
+    }
+}
