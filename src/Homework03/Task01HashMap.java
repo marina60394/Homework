b3d3b0af49facc8a07d310874c2e0fdc158b92e0
marina60394 <marina60394@gmail.com>
2019-01-30 09:15:41 +0200

    add Homework03

--- src/Homework03/Task01HashMap.java
+++ src/Homework03/Task01HashMap.java
@@ -1,13 +1,29 @@
 package Homework03;
 
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Marina on 29.01.2019.
  */
 public class Task01HashMap {
     public static void main(String[] args) {
-        HashMap<key, value> keyvalueHashMap  = new
-                
+        Map<String, Object> hashMap  = new HashMap<String, Object>();
+
+        hashMap.put("Test", 7);
+        hashMap.put("Class", 4.5);
+        hashMap.put("Cat", true);
+        hashMap.put("Dog", null);
+        hashMap.put("Bird", "Cat");
+        hashMap.put("Something", new Long(83));
+        hashMap.put("Mail", new Character('8'));
+        hashMap.put("Tab", '2');
+        hashMap.put("Browser", 22222222222L);
+        hashMap.put("IDE", (double) 489);
+
+        for (Map.Entry<String, Object> entry : hashMap.entrySet()) {
+            System.out.println(entry.getKey() + " - " + entry.getValue());
+        }
+
     }
 }
---
+++ src/Homework03/Task03CompareCollections.java
@@ -0,0 +1,60 @@
+package Homework03;
+
+import java.util.*;
+
+/**
+ * Created by Marina on 29.01.2019.
+ */
+public class Task03CompareCollections {
+    public static void main(String[] args) {
+        Set<String> hashSet = new HashSet<String>();
+        List<String> linkedList = new LinkedList<String>();
+
+        hashSet.add("pineapple");
+        hashSet.add("watermelon");
+        hashSet.add("apple");
+        hashSet.add("pear");
+        hashSet.add("quince");
+        hashSet.add("lemon");
+        hashSet.add("kiwi");
+
+        linkedList.add("plum");
+        linkedList.add("nut");
+        linkedList.add("apple");
+        linkedList.add("grape");
+        linkedList.add("peach");
+        linkedList.add("pear");
+        linkedList.add("cherry");
+        linkedList.add("orange");
+        linkedList.add("watermelon");
+        linkedList.add("apricot");
+        linkedList.add("pineapple");
+
+        Iterator<String> iteratorBeforeComparing = hashSet.iterator();
+
+        System.out.println("Hashset before comparing: ");
+        while (iteratorBeforeComparing.hasNext()){
+            System.out.println(iteratorBeforeComparing.next());
+        }
+
+        Iterator<String> itr = hashSet.iterator();
+        while (itr.hasNext()) {
+            if(linkedList.contains(itr.next())){
+                itr.remove();
+            }
+        }
+
+        System.out.println("\n" + "LinkedList: ");
+        for(int num=0; num<linkedList.size(); num++)
+        {
+            System.out.println(linkedList.get(num));
+        }
+
+        Iterator<String> iteratorAfterComparing = hashSet.iterator();
+        System.out.println("\n" + "Hashset after comparing: ");
+        while (iteratorAfterComparing.hasNext()){
+            System.out.println(iteratorAfterComparing.next());
+        }
+
+    }
+}
